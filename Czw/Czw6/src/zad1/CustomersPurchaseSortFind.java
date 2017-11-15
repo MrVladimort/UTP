e9a7319b06e0fe0bab9496e7956d7836a198491e
Vladyslav <mrvladimort@gmail.com>
2017-11-15 15:26:26 +0100

    czw6

--- Czw/Czw6/src/zad1/CustomersPurchaseSortFind.java
+++ Czw/Czw6/src/zad1/CustomersPurchaseSortFind.java
@@ -7,5 +7,33 @@
 package zad1;
 
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class CustomersPurchaseSortFind {
+    ArrayList<Purchase> purchases;
+
+    public void readFile(String fname) {
+        try {
+            Scanner sc = new Scanner(new FileReader(fname));
+            while (sc.hasNextLine()){
+                String [] purchase = sc.nextLine().split(";");
+                this.purchases.add(new Purchase(purchase[0], purchase[1], purchase[2],
+                        Double.parseDouble(purchase[3]), Double.parseDouble(purchase[4])));
+            }
+            System.out.println(this.purchases);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void showSortedBy(String nazwiska) {
+
+    }
+
+    public void showPurchaseFor(String id) {
+
+    }
 }
--- Czw/Czw6/src/zad1/Purchase.java
+++ Czw/Czw6/src/zad1/Purchase.java
@@ -8,4 +8,14 @@ package zad1;
 
 
 public class Purchase {
+    String id, customerName, towar;
+    double cena, ilosc;
+
+    Purchase(String id, String customerName, String towar, double cena, double ilosc){
+        this.id = id;
+        this.customerName = customerName;
+        this.towar = towar;
+        this.cena = cena;
+        this.ilosc = ilosc;
+    }
 }
